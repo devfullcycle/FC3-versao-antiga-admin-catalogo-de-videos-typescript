d3b7bb181bee9eec0e03e3d542a2f41217d2ed3d
Luiz Carlos <argentinaluiz@gmail.com>
2022-04-12 22:01:58 +0000

    feat: Add tests coverage and command to check app with TypeScript

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: '<rootDir>/../__coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -34,15 +34,24 @@ export default {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
+  //coverageReporters: [
+    //"json",
+    //"html"
   //   "json",
   //   "text",
   //   "lcov",
   //   "clover"
-  // ],
+  //],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    },
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
