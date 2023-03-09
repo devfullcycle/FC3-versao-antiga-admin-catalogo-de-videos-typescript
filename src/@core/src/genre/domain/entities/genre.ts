9fdb9e20eb8af0dc2d1079cb23d9bdc8013da1af
Luiz Carlos <argentinaluiz@gmail.com>
2023-03-09 11:53:07 -0300

    class: O que são agregados e relacionamentos de agregados com agregados e sub-entidades

--- src/@core/src/genre/domain/entities/genre.ts
+++ src/@core/src/genre/domain/entities/genre.ts
@@ -1,49 +1,75 @@
-import { HasMany } from "sequelize-typescript";
 import { UniqueEntityId } from "../../../@seedwork/domain";
 import { Category } from "../../../category/domain";
 
 class Genre {
-  id: number;
+  //Agregado - Raiz do agregado Aggregate root
+  id: string;
   name: string;
-  @HasMany()
-  categories: Category[];
+  categories: Category[]; //sub-entidade
   createdAt: Date;
-  updatedAt: Date;
+
+  constructor(props: GenreProps, id?: UniqueEntityId) {
+    this.id = id ? id.toString() : new UniqueEntityId().toString();
+    this.name = props.name;
+    this.categories = props.categories;
+    this.createdAt = props.createdAt;
+  }
+
+  addCategory(category: Category) {
+    this.categories.push(category);
+  }
+
+  removeCategory(categoryId: UniqueEntityId) {}
+
+  updateCategoryName(categoryId: UniqueEntityId, name: string) {
+    const category = this.categories.find(
+      (category) => category.id === categoryId
+    );
+    if (!category) throw new Error("Category not found");
+
+    category.update(name);
+  }
+}
+
+//Eager loading
+
+class Category {
+  //Agregado
+
+  public addXtp(){
+
+  }
 }
 
 new Genre();
 
 class Genre {
-    id: number;
-    name: string;
-    categories_id: UniqueEntityId[];
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  name: string;
+  categories_id: UniqueEntityId[];
+  createdAt: Date;
+  updatedAt: Date;
+
+
+    constructor(props: GenreProps, id?: UniqueEntityId) {
+        this.id = id ? id.toString() : new UniqueEntityId().toString();
+        this.name = props.name;
+        this.categories_id = props.categories_id;
+        this.createdAt = props.createdAt;
+    }
+
+    addCategory(categoryId: UniqueEntityId) {
+
+    }
+
+    removeCategory(categoryId: UniqueEntityId) {
+
+    }
 }
 
 new Genre();
 
+// o que são agregados
 
-class Pedido{
-    id: number;
-    cliente: Cliente;
-    itens: Item[];
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-class Item{
-    id: number;
-    produto: Produto;
-    quantidade: number;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-class Pedido{
-    id: number;
-    cliente_id: UniqueEntityId;
-    itens_id: UniqueEntityId[];
-    createdAt: Date;
-    updatedAt: Date;
-}
+// o volume de dados carregados
+// o tamanho da consistência
